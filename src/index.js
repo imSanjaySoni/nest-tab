53a6378169405f2e5b012a229df05ef31d2dd7b7
Afiur Rahman Fahim <afiur.fahim@gmail.com>
2018-02-25 19:28:13 +0600

    added ability to run and test landing page

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.css";
 import App from "./containers/App";
+// import LandingPage from "./containers/LandingPage"; // to run landing page in dev env
 import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<LandingPage />, document.getElementById("root"));
 registerServiceWorker();
